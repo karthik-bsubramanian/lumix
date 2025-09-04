6ca66a8e2af04bf6ccb1de24b500767b7a1ae50c
Karthik Balasubramanian <karthikbalasubramanian08@gmail.com>
2025-09-04 22:30:28 +0530

    setup appwrite as backend

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
-import Image from "next/image";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { client } from "@/lib/appwrite"
 
 export default function Home() {
+  async function ping(){
+    const response = await client.ping();
+    console.log(response);
+  }
   return (
   <div>
-
+      <Button onClick={ping}>
+          Send signal
+      </Button>
   </div>
   );
 }
