6ec899bb4e7e9f232839fafd6476bb1f7584e850
Karthik Balasubramanian <karthikbalasubramanian08@gmail.com>
2025-09-19 20:00:59 +0530

    remove appwrite baas

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,11 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { client } from "@/lib/appwrite"
 import { Navbar } from "@/components/Navbar";
 
 export default function Home() {
-  async function ping(){
-    const response = await client.ping();
-    console.log(response);
-  }
   return (
   <div>
     <Navbar/>
-      <Button onClick={ping}>
-          Send signal
-      </Button>
   </div>
   );
 }
