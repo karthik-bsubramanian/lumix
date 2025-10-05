cce6663dbf64f20ab3ae20c8aae7da36c2ebc032
Karthik Balasubramanian <karthikbalasubramanian08@gmail.com>
2025-10-06 00:48:32 +0530

    complete pod description

--- apps/lumix-FE/app/pod/[podname]/page.tsx
+++ apps/lumix-FE/app/pod/[podname]/page.tsx
@@ -3,6 +3,7 @@ import { PodBreadCrumb } from "@/components/PodBreadCrumb";
 import { Button } from "@/components/ui/button";
 import { Settings, Share2 } from "lucide-react";
 import { ThemeToggle } from "@/components/ThemeToggle";
+import { PodDescription } from "@/components/PodDescription";
 
 interface podPageProps {
     params: {
@@ -12,22 +13,27 @@ interface podPageProps {
 
 const pod: FC<podPageProps> = ({ params }) => {
     const { podname } = params;
-    return <div className="h-screen bg-background text-foreground">
+    return <div className="min-h-screen flex flex-col max-w-screen bg-background text-foreground">
         <header className="border-b border-border bg-card/50 backdrop-blur-sm">
             <div className="flex items-center justify-between px-6 py-2">
                 <PodBreadCrumb podName={podname} />
                 <div className="flex items-center space-x-3">
                     <ThemeToggle />
-                    <Button variant="outline" size="sm" className="hover:bg-green-500/50 dark:hover:bg-green-500/50 cursor-pointer">
+                    <Button variant="outline" size="sm" className="hover:bg-green-500/50 dark:hover:bg-green-500/50 hover:text-white dark:hover:text-black cursor-pointer">
                         <Share2 className="h-4 w-4 mr-2" />
                         Share
                     </Button>
-                    <Button variant="outline" size="sm" className="hover:bg-green-500/50 dark:hover:bg-green-500/50 cursor-pointer">
+                    <Button variant="outline" size="sm" className="hover:bg-green-500/50 dark:hover:bg-green-500/50 hover:text-white dark:hover:text-black cursor-pointer">
                         <Settings className="h-4 w-4" />
                     </Button>
                 </div>
             </div>
         </header>
+
+        <div className="flex-1 h-full">
+            <PodDescription podTitle={"Product innovation pod"} podDescription={"This is a idea discussion for the upcoming lumix project for our hackathon"} activeMembers={4}/>
+        </div>
+
     </div>
 }
 
---
+++ apps/lumix-FE/components/PodDescription.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import { useIsMobile } from "@/hooks/use-mobile"
+
+type podDescriptionProps = {
+    podTitle: string;
+    podDescription: string;
+    activeMembers: number;
+}
+export const PodDescription = ({ podTitle, podDescription, activeMembers }: podDescriptionProps) => {
+    const ismobile = useIsMobile();
+    return <div className="bg-gradient-to-r from-gray-50 to-green-50 dark:from-primary/10 dark:to-accent/10 px-8 py-2 items-center h-73px flex justify-between border-b border-border">
+        <div>
+            <h1 className="text-2xl font-semibold text-balance">{podTitle}</h1>
+            <p className="text-sm text-muted-foreground mt-1 text-pretty">{podDescription}</p>
+        </div>
+        <div className="rounded-2xl bg-green-500/50 px-2 text-white flex text-balance whitespace-nowrap">
+            {activeMembers} {ismobile ? "Active" : "Active members"}
+        </div>
+
+    </div>
+}
